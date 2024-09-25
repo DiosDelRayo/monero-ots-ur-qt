e27c0735d7c35ec70907fa8986b1e7444d921e98
who asks? <user@localhost>
2024-09-25 01:19:57 -0600

    Add check values to MoneroWalletData

--- monerodata/MoneroWalletData.h
+++ monerodata/MoneroWalletData.h
@@ -5,11 +5,15 @@
 
 class MoneroWalletData : public QObject {
     Q_OBJECT
-    Q_PROPERTY(QString address READ address WRITE setAddress FINAL)
-    Q_PROPERTY(QString spendKey READ spendKey WRITE setSpendKey FINAL)
-    Q_PROPERTY(QString viewKey READ viewKey WRITE setViewKey FINAL)
-    Q_PROPERTY(QString mnemonicSeed READ mnemonicSeed WRITE setMnemonicSeed FINAL)
-    Q_PROPERTY(int height READ height WRITE setHeight FINAL)
+    Q_PROPERTY(QString address READ address WRITE setAddress CONSTANT FINAL)
+    Q_PROPERTY(QString spendKey READ spendKey WRITE setSpendKey CONSTANT FINAL)
+    Q_PROPERTY(QString viewKey READ viewKey WRITE setViewKey CONSTANT FINAL)
+    Q_PROPERTY(QString mnemonicSeed READ mnemonicSeed WRITE setMnemonicSeed CONSTANT FINAL)
+    Q_PROPERTY(int height READ height WRITE setHeight CONSTANT FINAL)
+    Q_PROPERTY(bool isWallet READ isWallet CONSTANT FINAL)
+    Q_PROPERTY(bool isViewOnly READ isViewOnly CONSTANT FINAL)
+    Q_PROPERTY(bool isSeed READ isSeed CONSTANT FINAL)
+    Q_PROPERTY(bool isValid READ isValid CONSTANT FINAL)
 
 public:
     MoneroWalletData(
@@ -39,6 +43,12 @@ public:
 
     int height() { return m_height; }
     void setHeight(int value) { m_height = value; }
+
+    bool isWallet() { return !m_address.isEmpty() && !m_spend_key.isEmpty() && !m_view_key.isEmpty(); }
+    bool isViewOnly() { return m_spend_key.isEmpty() && !m_view_key.isEmpty() && !m_address.isEmpty(); }
+    bool isSeed() { return !m_mnemonic_seed.isEmpty() && m_spend_key.isEmpty() && m_view_key.isEmpty(); }
+    bool isValid() { return isWallet() || isViewOnly() || isSeed(); }
+    Q_INVOKABLE QStringList seedWords() { return m_mnemonic_seed.split(" "); }
 
 protected:
     QString m_address;
